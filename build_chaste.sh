def8a81189c18776e9e00d3685ff498e86350619
Ben Evans <ben.d.evans@gmail.com>
2017-12-04 11:13:40 +0000

    Adjust spacing

--- build_chaste.sh
+++ build_chaste.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -e
 
 # Must be run in the build directory e.g. /usr/chaste/build/
@@ -7,6 +6,7 @@ set -e
 NCORES=${1:-2}
 VERSION=${2:-'master'}
 SRC_DIR=${3:-/usr/chaste/src}
+
 if [ $# -ge 2 ]; then
     git clone -b $VERSION https://chaste.cs.ox.ac.uk/git/chaste.git $SRC_DIR
 fi
