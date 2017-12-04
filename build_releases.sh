23b40e8acce09a3787906e87c3b842f90bed33b8
Ben Evans <ben.d.evans@gmail.com>
2017-12-04 02:07:35 +0000

    Add script for automated building of releases

---
+++ build_releases.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VER=${1:-3.4.93221}
+BRANCH=release_$VER
+NCORES=${2:-4}
+docker build -t chaste:dependencies .
+
+#for each Dockerfile...
+# Automatically attach volume?
+docker build -t chaste:$VER --build-arg CHASTE_VERSION=$BRANCH \
+                            --build-arg NCORES=$NCORES \
+                            -f Dockerfile_Release .
+
+# TODO: Push to docker hub
+
+docker run -it -v $(pwd):/usr/chaste/src/projects chaste:$VER
