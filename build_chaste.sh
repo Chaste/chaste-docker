6d3400d23727047094d56739560e39209d2d0a9c
Ben Evans <ben.d.evans@gmail.com>
2017-12-24 15:13:04 +0000

    Add print outs

--- build_chaste.sh
+++ build_chaste.sh
@@ -11,11 +11,16 @@ GIT_REMOTE=${5:-https://github.com/Chaste/Chaste.git}
 
 
 if [ $# -ge 2 ] && [ $VERSION != '-' ]; then
+    echo "Cloning Chaste from $GIT_REMOTE#$VERSION into $SOURCE_DIR..."
     git clone -b $VERSION $GIT_REMOTE $SOURCE_DIR
 fi
+
+echo "Building Chaste $VERSION in $BUILD_DIR with $NCORES cores..."
 cmake -DCMAKE_BUILD_TYPE:STRING=Release \
       -DChaste_ERROR_ON_WARNING:BOOL=OFF \
       -DChaste_UPDATE_PROVENANCE:BOOL=OFF \
       -B$BUILD_DIR \
       -H$SOURCE_DIR && \
 make -j $NCORES -C $BUILD_DIR # -f $BUILD_DIR/Makefile
+echo "Done!"
+echo "New projects may be initialised with the provided script new_project.sh"
