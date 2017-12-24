e4ba592cc33e6755e6ef4550e76a278e9b336df1
Ben Evans <ben.d.evans@gmail.com>
2017-12-24 15:12:11 +0000

    Add more environment and build variables

--- build_chaste.sh
+++ build_chaste.sh
@@ -5,10 +5,17 @@ VERSION=${1:-master}
 NCORES=${2:-2}
 CHASTE_DIR=/home/chaste
 SOURCE_DIR=${3:-$CHASTE_DIR/src}
+BUILD_DIR=${4:-$CHASTE_DIR/lib}
+GIT_REMOTE=${5:-https://github.com/Chaste/Chaste.git}
+#               https://chaste.cs.ox.ac.uk/git/chaste.git
 
 
 if [ $# -ge 2 ]; then
-    git clone -b $VERSION https://chaste.cs.ox.ac.uk/git/chaste.git $SRC_DIR
+    git clone -b $VERSION $GIT_REMOTE $SOURCE_DIR
 fi
-cmake $SRC_DIR
+cmake -DCMAKE_BUILD_TYPE:STRING=Release \
+      -DChaste_ERROR_ON_WARNING:BOOL=OFF \
+      -DChaste_UPDATE_PROVENANCE:BOOL=OFF \
+      -B$BUILD_DIR \
+      -H$SOURCE_DIR && \
 make -j $NCORES -C $BUILD_DIR # -f $BUILD_DIR/Makefile
