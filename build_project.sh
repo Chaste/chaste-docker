c9538188ce4276d279c5a71bfad03738eaf42c57
Ben Evans <ben.d.evans@gmail.com>
2018-01-15 20:04:57 +0000

    Try to build from non-build directory

--- build_project.sh
+++ build_project.sh
@@ -12,9 +12,15 @@ fi
 CMAKE_FLAG=${2:-"n"}
 NCORES=${3:-$(nproc)}
 SRC_DIR=${4:-/home/chaste/src}
+BUILD_DIR=${5:-/home/chaste/lib}
 
 if [ "$CMAKE_FLAG" = "c" ]; then
-    cmake $SRC_DIR  # Only run if new files have been created
+    #cmake $SRC_DIR  # Only run if new files have been created
+    cmake -DCMAKE_BUILD_TYPE:STRING=Release \
+          -DChaste_ERROR_ON_WARNING:BOOL=OFF \
+          -DChaste_UPDATE_PROVENANCE:BOOL=OFF \
+          -B$BUILD_DIR \
+          -H$SRC_DIR
 fi
-make -j$NCORES $1
+make -j$NCORES -C $BUILD_DIR $1 # -f $BUILD_DIR/Makefile
 ctest -V -R $1
