5f22f40009c0cb967051aa315a19d4ad3d6be690
Ben Evans <ben.d.evans@gmail.com>
2024-01-19 14:34:45 +0000

    Rely on environment variables for different build types and only warn if they are mismatched

--- scripts/build_chaste.sh
+++ scripts/build_chaste.sh
@@ -18,18 +18,35 @@ fi
 
 echo "Building Chaste $VERSION in $CHASTE_BUILD_DIR with $NCORES cores..."
 if [ $VERSION = 'main' ] || [ $VERSION = 'release' ]; then
-    cmake -DCMAKE_BUILD_TYPE:STRING=Release \
-          -DChaste_ERROR_ON_WARNING:BOOL=OFF \
-          -DChaste_UPDATE_PROVENANCE:BOOL=ON \
-          -H$CHASTE_SOURCE_DIR \
-          -B$CHASTE_BUILD_DIR
+#     cmake -DCMAKE_BUILD_TYPE:STRING=Release \
+#           -DChaste_ERROR_ON_WARNING:BOOL=OFF \
+#           -DChaste_UPDATE_PROVENANCE:BOOL=ON \
+#           -H$CHASTE_SOURCE_DIR \
+#           -B$CHASTE_BUILD_DIR
+    if [ "$CMAKE_BUILD_TYPE" != "Release" ] || [ "$Chaste_ERROR_ON_WARNING" != "OFF" ]; then
+        echo "WARNING: Chaste ${VERSION} branch should be built with Release and ERROR_ON_WARNING=OFF."
+        echo "The environment variables are currently set as: "
+        echo "CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+        echo "Chaste_ERROR_ON_WARNING=${Chaste_ERROR_ON_WARNING}"
+    fi
 else # if [ $VERSION = 'develop' ]; then
-    cmake -DCMAKE_BUILD_TYPE:STRING=Debug \
-          -DChaste_ERROR_ON_WARNING:BOOL=ON \
-          -DChaste_UPDATE_PROVENANCE:BOOL=OFF \
-          -H$CHASTE_SOURCE_DIR \
-          -B$CHASTE_BUILD_DIR
+#     cmake -DCMAKE_BUILD_TYPE:STRING=Debug \
+#           -DChaste_ERROR_ON_WARNING:BOOL=ON \
+#           -DChaste_UPDATE_PROVENANCE:BOOL=OFF \
+#           -H$CHASTE_SOURCE_DIR \
+#           -B$CHASTE_BUILD_DIR
+        echo "WARNING: Chaste ${VERSION} branch should be built with Debug and ERROR_ON_WARNING=ON."
+        echo "The environment variables are currently set as: "
+        echo "CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+        echo "Chaste_ERROR_ON_WARNING=${Chaste_ERROR_ON_WARNING}"
 fi
+
+# Only run if new files have been created
+cmake -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} \
+        -DChaste_ERROR_ON_WARNING:BOOL=${Chaste_ERROR_ON_WARNING} \
+        -DChaste_UPDATE_PROVENANCE:BOOL=${Chaste_UPDATE_PROVENANCE} \
+        -H$CHASTE_SOURCE_DIR \
+        -B$CHASTE_BUILD_DIR
 
 make --no-print-directory -j$NCORES -C $CHASTE_BUILD_DIR # -f $CHASTE_BUILD_DIR/Makefile
 echo "Done!"
