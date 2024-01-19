f7b1bacfc8a0b305b25ee958f0df6cfacf32e0f6
Ben Evans <ben.d.evans@gmail.com>
2024-01-19 14:34:52 +0000

    Tidy up

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,14 +13,10 @@ fi
 echo "Building and running $TEST_SUITE tests..."
 echo "Test outputs will be written to: $CHASTE_TEST_OUTPUT"
 
-
 if [ "$CMAKE_FLAG" = "c" ]; then
     # Only run if new files have been created
     cmake -H$CHASTE_SOURCE_DIR \
           -B$CHASTE_BUILD_DIR
-          # -DCMAKE_BUILD_TYPE:STRING=Release \
-          # -DChaste_ERROR_ON_WARNING:BOOL=OFF \
-          # -DChaste_UPDATE_PROVENANCE:BOOL=OFF \
 else
     echo "Skipping cmake. If the tests fail to build, try rerunning with:"
     echo "`basename "$0"` $TEST_SUITE c"
